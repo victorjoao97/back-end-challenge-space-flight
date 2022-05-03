ff95d3f953430ed75bc933b275b16765d10d17cf
João Victor Nascimento <dev.joao.vn@outlook.com>
2022-05-03 00:48:35 -0300

    refac: teste findall

--- src/article/article.controller.spec.ts
+++ src/article/article.controller.spec.ts
@@ -1,20 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ArticleController } from './article.controller';
 import { ArticleService } from './article.service';
+import { RepositoryModule } from './repository/repository.module';
+import { RepositoryService } from './repository/repository.service';
 
 describe('ArticleController', () => {
   let controller: ArticleController;
-
+  let repository: RepositoryService;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ArticleController],
       providers: [ArticleService],
+      imports: [RepositoryModule],
     }).compile();
 
     controller = module.get<ArticleController>(ArticleController);
+    repository = module.get<RepositoryService>(RepositoryService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should be findAll return mock', () => {
+    jest.spyOn(repository, 'findAll').mockImplementation(() => []);
+    expect(controller.findAll()).toHaveLength(0);
   });
 });
